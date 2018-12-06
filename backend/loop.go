e06bde5c4295cb3f1a15f3f861702b2b6f73adab
Farbod Salimi <farbod_salimi@yahoo.com>
2018-12-05 22:20:07 -0800

    Add ioloop

---
+++ backend/loop.go
@@ -0,0 +1,27 @@
+package backend
+
+import (
+	"../workers"
+)
+
+type IOLoop struct {
+	StopSignal  bool
+	QueueWorker SQSWorker
+	Worker      workers.FutureWorker
+}
+
+func (iol IOLoop) RunOnce() {
+	messages := iol.QueueWorker.FetchMessages()
+	if len(messages) > 0 {
+		processedMessages := iol.Worker.ProcessMessages(messages)
+		if len(processedMessages) > 0 {
+			iol.QueueWorker.DeleteMessages(processedMessages)
+		}
+	}
+}
+
+func (iol IOLoop) Run() {
+	for iol.StopSignal == false {
+		iol.RunOnce()
+	}
+}
